5d37bbfa16f662ae301c35b0ba18210e205fad82
Eduardo <edygomezgg4@gmail.com>
2021-06-17 17:21:44 -0500

    Added typescript and finished useCounter

---
+++ src/hooks/useCounter/README.md
@@ -0,0 +1,15 @@
+# Use Counter
+
+It is a counter with some utility functions
+
+```
+increment(factor)
+decrement(factor)
+reset()
+```
+
+Ejemplo de uso: 
+
+```bash
+{counter, increment, decrement, reset} = useCounter(initialNumber)
+```
---
+++ src/hooks/useCounter/useCounter.ts
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+type Counter = {
+    counter : number, 
+    increment : Function, 
+    decrement : Function, 
+    reset : Function
+}
+
+export const useCounter = (initialValue : number = 0) : Counter => {
+    
+    const [counter, setState] = useState(initialValue)
+
+    const increment = (factor : number = 1) : void => { setState(counter + factor) }
+    const decrement = (factor : number = 1) : void => { setState(counter - factor) }
+    const reset = () : void => { setState(initialValue) }
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset
+    }
+}
