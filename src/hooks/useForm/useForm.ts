b2a9da3421f24bab7294eba2290ce7b0d7dd419f
G00Z_G00Z <73507446+G00Z-G00Z@users.noreply.github.com>
2021-07-23 13:49:44 -0500

    Update useForm.ts

--- src/hooks/useForm/useForm.ts
+++ src/hooks/useForm/useForm.ts
@@ -1,41 +1,40 @@
-import React, { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
-type formValues = {[key: string] : any}
+type formValues = { [key: string]: any };
 
 type formData = {
-    values : formValues, 
-    handleInputChange : Function, 
-    resetForm : Function
-}
+	values: formValues;
+	handleInputChange: Function;
+	resetForm: Function;
+};
 
-export const useForm = (initialState : formValues = {} ) : formData => {
+export const useForm = (initialState: formValues = {}): formData => {
+	const [values, setValues] = useState(initialState);
 
-    const [values, setValues] = useState(initialState)
+	const resetForm = (): void => {
+		setValues(initialState);
+	};
 
-    const resetForm = () : void => {
-        setValues(initialState)
-    }
+	interface inputWithNames
+		extends Omit<React.FormEvent<HTMLInputElement>, "target"> {
+		target: {
+			name: string;
+			value: any;
+		};
+	}
 
-    interface inputWithNames {
-        target : {
-            name : string, 
-            value : any
-        }, 
-        preventDefault : Function
-    }
+	const handleInputChange = (e: inputWithNames) => {
+		e.preventDefault();
+		const target: {
+			name: string;
+			value: string;
+		} = e.target;
+		setValues({
+			...values,
+			[target.name]: target.value,
+		});
+	};
 
-    const handleInputChange = (e :inputWithNames) => {
-        e.preventDefault()
-        const  target  :  {
-            name : string, 
-            value : string}
-         = e.target
-        setValues({
-            ...values,
-            [target.name]: target.value
-        })
-    }
-
-
-    return { values, handleInputChange, resetForm }
-}
+	return { values, handleInputChange, resetForm };
+};
